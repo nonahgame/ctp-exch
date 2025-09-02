503613aae2bfaf4d5ad1d1d1e377c63942ef18a1
nonahgame <103342324+nonahgame@users.noreply.github.com>
2025-09-02 14:05:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -397,10 +397,10 @@ def ai_decision(df, stop_loss_percent=STOP_LOSS_PERCENT, take_profit_percent=TAK
             action = "sell"
 
     if action == "hold" and position is None:
-        if (kdj_j < - 54.00 and ema1 < ema2 or kdj_j < kdj_d and macd < macd_signal and rsi < 19.00):
+        if (kdj_j < - 14.00 and ema1 > ema2 or kdj_j > kdj_d and macd > macd_signal and rsi < 19.00):
             logger.info(f"Buy condition met: kdj_j={kdj_j:.2f}, kdj_d={kdj_d:.2f}, close={close_price:.2f}, open={open_price:.2f}, ema1={ema1:.2f}, ema2={ema2:.2f}")
             action = "buy"
-        elif (lst_diff > 8.00 and kdj_j < 10.00 and rsi < 27.00): # and kdj_j > kdj_d 
+        elif (lst_diff > 8.00 and kdj_j < 5.00 and rsi < 27.00): # and kdj_j > kdj_d 
             logger.info("Buy-logic triggered.")
             action = "buy"
         elif (macd > macd_signal and ema1 > ema2 and lst_diff > 7.00): # and kdj_j > kdj_d 
